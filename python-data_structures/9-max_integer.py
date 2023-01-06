716c2a84f4395c4d49195b7d7da220290b39e630
SnehaKallem <snehak@brightcomgroup.com>
2023-01-05 21:02:56 -0800

    task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,8 +2,8 @@
 def max_integer(my_list=[]):
     if my_list == " ":
         return "None"
-    big_value = my_list[0]
-    for i in range(len(my_list)):
-        if my_list[i] > big_value:
-            big_value = my_list[i]
+    big_value=0
+    for i in my_list:
+        if i > big_value:
+            big_value=i
     return (big_value)
