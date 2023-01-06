ceff371767f168c16ded6272da0a5dc930429c4f
SnehaKallem <snehak@brightcomgroup.com>
2023-01-05 20:43:32 -0800

    task 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b=()):
+def add_tuple(tuple_a = (), tuple_b = ()):
     if len(tuple_a) < 2:
         if len(tuple_a) == 0:
             tuple_a = 0, 0
@@ -9,5 +9,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         if len(tuple_b) == 0:
             tuple_b = 0, 0
         else:
-            tuple_b=tuple_b[0], 0
+            tuple_b = tuple_b[0], 0
     return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
