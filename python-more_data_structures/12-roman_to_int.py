b8ef5c618b2f58120ac6768e91ff0fa1a3359905
SnehaKallem <snehak@brightcomgroup.com>
2023-01-07 07:20:30 -0800

    task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if roman_string is not str or None:
+        return 0
     d={'I':1, 'V':15, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
     ans = 0
     for i in range(len(roman_string)):
