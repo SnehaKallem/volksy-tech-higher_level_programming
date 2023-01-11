7f1474f31211df960e7413edf09a1b5ac01221d3
SnehaKallem <snehak@brightcomgroup.com>
2023-01-10 20:53:33 -0800

    task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""String size"""
+
+
 class Square:
+    """size"""
     def __init__(self, size=0):
         if type(size) != int:
             raise TypeError("size must be an integer")
