67d1ecef86822b54461d62501615ee099ce22937
SnehaKallem <snehak@brightcomgroup.com>
2023-01-11 01:40:17 -0800

    task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -5,7 +5,7 @@
 class Square:
     """srea of square"""
 
-    def __init__(self, size):
+    def __init__(self, size=0):
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
