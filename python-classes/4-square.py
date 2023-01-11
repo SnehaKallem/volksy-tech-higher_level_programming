dfde1be87bfe38267975ee42bb15a1bfdb57da64
SnehaKallem <snehak@brightcomgroup.com>
2023-01-11 02:47:24 -0800

    task 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -19,6 +19,5 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
     def area(self):
         return self.__size * self.__size
