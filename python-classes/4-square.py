151faa241187563690a9117a7056f011349cd755
SnehaKallem <snehak@brightcomgroup.com>
2023-01-11 02:35:39 -0800

    task 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,12 +5,11 @@
 class Square:
     """string size"""
 
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         self.size = size
     @property
     def size(self):
         return self.__size
-
     @size.setter
     def size(self, value):
         if type(value) != int:
@@ -18,7 +17,5 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size=value
-
-
     def area(self):
         return self.__size * self.__size
