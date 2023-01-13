0ba8c78f22500c219b2f3edd5e683333a0667a30
SnehaKallem <snehak@brightcomgroup.com>
2023-01-13 01:40:56 -0800

    task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Square"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A class named Square"""
+    def __init__(self, size):
+    
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
