fdd9159fcd8fc0d3d16a9abbe702407f1b12b984
SnehaKallem <snehak@brightcomgroup.com>
2023-01-12 23:39:30 -0800

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module creates a class named BaseGeometry"""
+
+
+class BaseGeometry:
+    """class"""
+
+    def area(self):
+        """raises an exception"""
+        raise Exception("area() is not implemented")
