5f3f63e90619c92ffeca19c952a94bd5d02df0dc
SnehaKallem <snehak@brightcomgroup.com>
2023-01-15 02:47:53 -0800

    task 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -3,31 +3,36 @@
 
 
 class Rectangle:
-    """width"""
-    @property
-    def width(self):
-        return self.__width
-    
-    @width.setter
-    def width(self, value):
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            raise TypeError("width must be >= 0")
-        self.__width = value
-    
+    """width and height"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
     @property
     def height(self):
+        """Gets the height of the class instance"""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
+        """Sets the height of the class instance"""
         if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
-            raise TypeError("height must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
-    
-    def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+
+    @property
+    def width(self):
+        """Gets the width of the class instance"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the width of the class instance"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
