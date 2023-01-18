eaad256e2ef900a26528ddf0710f92446aad4942
SnehaKallem <snehak@brightcomgroup.com>
2023-01-18 02:05:48 -0800

    task 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""string"""
+
+
+def write_file(filename="", text=""):
+    """num of char"""
+    character=0
+    with open(filename, encoding='UTF-8'):
+        for line in file:
+            character = character + 1
+        return character
