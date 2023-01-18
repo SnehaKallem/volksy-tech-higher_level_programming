134802032635afb66d97f559446cbfcd6bf50d26
SnehaKallem <snehak@brightcomgroup.com>
2023-01-18 02:46:40 -0800

    task 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -4,6 +4,6 @@
 
 def append_write(filename="", text=""):
     """with"""
-    with open(filename, 'w', encoding="utf-8") as f:
+    with open(filename, 'a', encoding="utf-8") as f:
         f.write(text)
         return len(text)
