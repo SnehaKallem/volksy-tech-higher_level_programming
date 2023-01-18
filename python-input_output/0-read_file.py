f04bb6edb7b7a29ef00e499857ac6c9a48bc9b80
SnehaKallem <snehak@brightcomgroup.com>
2023-01-18 01:09:58 -0800

    task 0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""read text file"""
+
+
+def read_file(filename=""):
+    """with"""
+    with open(filename, encoding = UTF8) as file:
+        for line in file:
+            print(line, end="")
