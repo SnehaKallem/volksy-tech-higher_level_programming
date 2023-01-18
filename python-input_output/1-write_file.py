0abad0e7941b07b62f538d181169c30ec82c58f2
SnehaKallem <snehak@brightcomgroup.com>
2023-01-18 02:07:08 -0800

    task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,7 +5,7 @@
 def write_file(filename="", text=""):
     """num of char"""
     character=0
-    with open(filename, encoding='UTF-8'):
+    with open(filename, encoding='UTF-8') as file:
         for line in file:
             character = character + 1
         return character
