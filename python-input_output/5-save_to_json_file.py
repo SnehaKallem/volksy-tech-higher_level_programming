faea5d9f00d0fa54a5fd4ede78fbcda5e06c8a29
SnehaKallem <snehak@brightcomgroup.com>
2023-01-18 03:49:38 -0800

    task 5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """with"""
-    with open(filename, 'w' , encoding="UTF-8") as f:
+    with open(filename, 'w', encoding="UTF-8") as f:
         json.dump(my_obj, f)
