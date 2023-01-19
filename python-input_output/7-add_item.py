6a97c20c1186b2ffc0ffd7f692defedd588974a5
SnehaKallem <snehak@brightcomgroup.com>
2023-01-18 20:52:39 -0800

    task 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""module"""
+
+
+from os, sys import path, argv
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+if path.exists("add_item.json") is False:
+    save_to_json_file([], "add_item.json")
+my_list = load_from_json_file("add_item.json")
+
+for i in range(1, len(argv)):
+    my_list.append(argv[i])
+save_to_json_file(my_list, "add_item.json")
