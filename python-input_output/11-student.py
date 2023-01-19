7069046eed816a590be5668e9f13a7bdae675f0e
SnehaKallem <snehak@brightcomgroup.com>
2023-01-18 23:17:50 -0800

    task 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""creates a class"""
+
+
+class Student:
+    """A class named Student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes an instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves dictionary representation of student"""
+        if type(attrs) is list and all(type(x) is str for x in attrs):
+            dictionary = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    dictionary[i] = self.__dict__[i]
+            return dictionary
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of a Student instance"""
+        for key, value in json.items():
+            self.__dict__[key] = value
