14fcc8d24e36545cab868b1d06d06890c32b5243
SnehaKallem <snehak@brightcomgroup.com>
2023-01-18 23:02:11 -0800

    task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""class"""
+
+
+class Student:
+    """class"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes an instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves dictionary representation of student"""
+        if type(attrs) is list and all(type(x) is str for x in attrs):
+            dictionary = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    dictionary[i] = self.__dict__[i]
+            return dictionary
+        else:
+            return self.__dict__
