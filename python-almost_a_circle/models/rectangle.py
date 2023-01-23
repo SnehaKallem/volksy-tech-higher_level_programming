f3c99bd06c8d4491db97ab94d33a30a1e25cc6cb
SnehaKallem <snehak@brightcomgroup.com>
2023-01-23 02:17:02 -0800

    task 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,67 +1,58 @@
 #!/usr/bin/python3
-"""
-rectangle module that defines the Rectangle class
-"""
+"""This module creates the Rectangle class that inherits from Base"""
+
+
 from models.base import Base
 
 
 class Rectangle(Base):
-    """Rectangle class"""
-    def _init_(self, width, height, x=0, y=0, id=None):
-        """Initializer for Rectangle class"""
-        super()._init_(id)
+    """A class named Rectangle
+    Attributes:
+    attr1(id): id of object
+    attr2(width): rectangle width
+    attr3(height): rectangle height
+    attr4(x): number of spaces before rectangle
+    attr5(y): number of newlines before rectangle
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializes instance of Rectangle class"""
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
 
     def area(self):
-        """method to calculate area of a rectangle"""
-        return self._width * self._height
+        """Returns the area value of the Rectangle instance"""
+        return self.__width * self.__height
 
     def display(self):
-        """method to display a rectangle using `#`"""
-        print("\n" * (self.__y), end="")
-        for i in range(self.__height):
-            print(" " * self._x + "#" * self._width)
+        """Prints in stdout the instance with the character #"""
+        for row in range(self.__y):
+            print()
+        for row in range(self.__height):
+            for column in range(self.__x):
+                print(" ", end="")
+            for column in range(self.__width):
+                print("#", end="")
+            print()
 
-    def _str_(self):
-        """return string representation of Rectangle"""
-        return '[' + type(self)._name_ + '] (' + str(self.id) \
-            + ') ' + str(self._x) + '/' + str(self._y) + ' - ' \
-            + str(self._width) + '/' + str(self._height)
+    def __str__(self):
+        """Returns the informal representation of the object"""
+        return"[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format(
+            self.id, self.__x, self.__y, self.__width, self.__height)
 
-    def update(self, *args, **kwargs):
-        """assigns an argument to each attribute"""
-        key = ["id", "width", "height", "x", "y"]
-        for i in range(len(args)):
-            setattr(self, key[i], args[i])
-        for key, value in kwargs.items():
-            setattr(self, key, value)
 
-    def to_dictionary(self):
-        """
-        Returns dictionary representation of
-        Rectangle instance
-        """
-        selfDict = {
-            'x': self.x,
-            'y': self.y,
-            'id': self.id,
-            'height': self.height,
-            'width': self.width
-        }
-        return selfDict
 
     @property
     def width(self):
-        """Getter for __width"""
+        """Returns width of instance"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Setter for __width"""
-        if type(value) != int:
+        """Sets width of class instance"""
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -69,13 +60,13 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        """Getter for __height"""
+        """Returns height of instance"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Setter for __height"""
-        if type(value) != int:
+        """Sets height of class instance"""
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -83,13 +74,13 @@ class Rectangle(Base):
 
     @property
     def x(self):
-        """Getter for __x"""
+        """Returns x of instance"""
         return self.__x
 
     @x.setter
     def x(self, value):
-        """Setter for __x"""
-        if type(value) != int:
+        """Sets x of class instance"""
+        if type(value) is not int:
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
@@ -97,13 +88,13 @@ class Rectangle(Base):
 
     @property
     def y(self):
-        """Getter for __y"""
+        """Returns y of instance"""
         return self.__y
 
     @y.setter
     def y(self, value):
-        """Setter for __y"""
-        if type(value) != int:
+        """Sets y of class instance"""
+        if type(value) is not int:
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
