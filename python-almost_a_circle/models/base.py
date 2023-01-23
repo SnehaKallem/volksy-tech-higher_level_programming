02e4371225140810cbb5cd25fd72503643b42f8b
SnehaKallem <snehak@brightcomgroup.com>
2023-01-23 01:40:59 -0800

    task 1

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -5,9 +5,8 @@
 class Base():
     """base class for checking id for other classes"""
 
-    __nb_objects = 0
-    
-    def __int__(self, id=None):
+    __nb_objects = 0 
+    def __init__(self, id=None):
         """base class with id"""
         if id is not None:
             self.id = id
