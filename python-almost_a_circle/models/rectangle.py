b815aef4bb1f162c81d0e1dc68b738aa1d36e138
SnehaKallem <snehak@brightcomgroup.com>
2023-01-23 01:54:12 -0800

    task 2

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module creates the Rectangle class that inherits from Base"""
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """class rectangle"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializes instance of Rectangle class"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
