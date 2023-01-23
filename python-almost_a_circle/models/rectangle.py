21e9a36a470b6840adc8240fd1ac56b23738d90d
SnehaKallem <snehak@brightcomgroup.com>
2023-01-23 01:56:28 -0800

    task 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -14,3 +14,32 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
+
+    @property
+    def width(self):
+        """Returns width of instance"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets width of class instance"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Returns height of instance"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets height of class instance"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
