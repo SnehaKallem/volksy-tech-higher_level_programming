08df0a9852f945d8c47ebd8cc7c06acdc0c2c048
SnehaKallem <snehak@brightcomgroup.com>
2023-01-29 23:02:42 -0800

    task 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,3 +1,3 @@
 -- Create second table
-CREATE TABLE second_table(id INT,name VARCHAR(256),score INT);
+CREATE TABLE IF EXISTS second_table(id INT,name VARCHAR(256),score INT);
 INSERT INTO second_table VALUES(1,'John',10),(2,'Alex',3),(3,'Bob',14),(4,'George',8);
