a9103635e8414d62cc35dc2c674e3e1bacf71a2d
SnehaKallem <snehak@brightcomgroup.com>
2023-02-01 22:38:35 -0800

    task 10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
 -- dump database and display in descending order oftv shows
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows RIGHT JOIN tv_show_genres ON tv_shows_genres.genre_id = tv_shows.id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows RIGHT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_shows.id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
