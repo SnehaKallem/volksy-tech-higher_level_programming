3cad99f6ca767f0f8793b82243ab3ec8cfcd3cd5
SnehaKallem <snehak@brightcomgroup.com>
2023-02-01 22:55:08 -0800

    task 11

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,2 @@
+-- show list of table 
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows RIGHT JOIN tv_show_genres WHERE genre IS NOT NULL ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
