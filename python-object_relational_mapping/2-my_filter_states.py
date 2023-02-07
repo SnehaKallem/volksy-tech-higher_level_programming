4e88360c3df795196c1eb9014dd4288224b41413
SnehaKallem <snehak@brightcomgroup.com>
2023-02-07 02:07:06 -0800

    task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -7,7 +7,7 @@ import MySQLdb
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     c = conn.cursor()
     c.execute(
             "SELECT * from states WHERE name LIKE BINARY '{}' ORDER BY id ASC"
