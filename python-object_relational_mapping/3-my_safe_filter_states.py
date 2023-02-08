25d5abca8561e3bec8ab03f89aa7b8abc6d91fc4
SnehaKallem <snehak@brightcomgroup.com>
2023-02-07 22:14:11 -0800

    task 3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,7 +4,7 @@
 import sys
 import MySQLdb
 
-if __name__ == __main__:
+if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2],
             db=sys.argv[3], charset="utf8")
     c = conn.cursor()
@@ -13,5 +13,4 @@ if __name__ == __main__:
     for i in a:
         print(i)
     c.close()
-    conn.close()
-    
+    conn.close()    
