492b10ca9ba31d64d4d438a9a2493fb4e07cdd82
SnehaKallem <snehak@brightcomgroup.com>
2023-02-08 01:22:53 -0800

    task 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     c = conn.cursor()
-    c.execute("SELECT cities.name FROM cities" + 
+    c.execute("SELECT cities.name FROM cities" +
               " INNER JOIN states ON cities.state_id = states.id" +
               " WHERE states.name = %s ORDER BY cities.id ASC",
               [sys.argv[4]])
