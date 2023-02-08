a2add413606ca83a7e69d40b01d159fc8f032ea0
SnehaKallem <snehak@brightcomgroup.com>
2023-02-07 20:28:53 -0800

    task 3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""name"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == __main__:
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    c = conn.cursor()
+    c.execute("SELECT * FROM states WHERE name = {} ORDER BY id ASC".format(sys.argv[4])
+    a = c.fetchall()
+    for i in a:
+        print(i)
+    c.close()
+    conn.close()
+    
